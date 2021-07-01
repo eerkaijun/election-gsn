51a849220d9b2ab29c721268cf99b82df481e708
Kai Jun Eer <kaijuneer@gmail.com>
2021-07-01 14:37:59 +0100

    upgrade to solidity 0.8.0 and above

--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.7.0 <0.8.0;
+pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Vote is Ownable {
 
