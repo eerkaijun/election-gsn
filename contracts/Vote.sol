ae9b4ac907efcac60d6b90a27473b5a62b1d9847
Kai Jun Eer <kaijuneer@gmail.com>
2021-07-01 14:13:37 +0100

    added voting functionality

--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -15,15 +15,24 @@ contract Vote is Ownable {
   }
 
   constructor() {
+    addCandidate("Donald Trump");
     addCandidate("Kanye West");
   }
 
   uint candidatesCount = 0;
   mapping(uint => Candidate) public candidates;
+  mapping(address => bool) public voters;
 
   function addCandidate(string memory _name) private {
     candidates[candidatesCount] = Candidate(_name, 0);
     candidatesCount++;
+  }
+
+  function vote(uint _candidateID) public {
+    require(!voters[msg.sender]);
+    require(_candidateID >= 0 && _candidateID < candidatesCount);
+    voters[msg.sender] = true;
+    candidates[_candidateID].voteCount++;
   }
 
 }
