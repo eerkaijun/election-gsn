00f2a0fc8a143bfddfc8d7849366b79324976207
eerkaijun <kaijuneer@gmail.com>
2021-07-20 05:51:14 +0100

    add registration frontend

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.jpg">
-    <HelloWorld msg="Welcome to the e-voting system"/>
+    <Registration v-if="registered==false" msg="Please register yourself"/>
+    <HelloWorld v-if="registered" msg="Welcome to the e-voting system"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/Homepage.vue'
+import HelloWorld from './components/Homepage.vue';
+import Registration from './components/Registration.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Registration
+  },
+  data() {
+    return {
+      registered: false
+    }
+  },
+
 }
 </script>
 
--- app/src/components/Registration.vue
+++ app/src/components/Registration.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h3>Candidates</h3>
-    <ul>
-      <li>{{ candidate1 }}<br><button v-on:click="vote(0)">Vote</button></li>
-      <li>{{ candidate2 }}<br><button v-on:click="vote(1)">Vote</button></li>
-    </ul>
+    <label>Name: </label>
+    <input type="text" v-model="name"><br><br>
+    <label>National ID: </label>
+    <input type="text" v-model="nationalID"><br><br>
+    <button v-on:click="register()">Register</button>
     <h3>Ecosystem</h3>
     <ul>
       <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
@@ -21,35 +21,21 @@
 
 import VoteABI from '../abi/Vote';
 const Web3 = require('web3');
-const { RelayProvider } = require('@opengsn/provider');
-
-// Ropsten accept-everything paymaster (pre-deployed)
-//const paymasterAddress = "0x05319d82fa69EA8434A967CdF4A2699Db4Ff40e8";
-// Mumbai accept-everything paymaster (pre-deployed)
-//const paymasterAddress = "0xcA94aBEdcC18A10521aB7273B3F3D5ED28Cf7B8A";
-// Mumbai accept-everything paymaster (self-deployed)
-const paymasterAddress = "0x627Ed19358C1b8194eb49c6e7Cfe538Be1C73d2c";
-const gsnConfig = {
-    paymasterAddress,
-    loggerConfiguration: {
-        logLevel: 'debug',
-        loggerUrl: 'logger.opengsn.org',
-    }
-};
-const useGSN = false; //boolean to indicate whether user needs to pay their own gas
 
 export default {
-  name: 'HelloWorld',
+  name: 'Registration',
   props: {
-    msg: String
+    msg: String,
+    registered: Boolean
   },
   data() {
     return {
       web3: null,
       contract: null,
       account: '0x0',
-      candidate1: '',
-      candidate2: ''
+      display: '',
+      name: '',
+      nationalID: ''
     }
   },
 
@@ -63,16 +49,7 @@ export default {
     async initProvider() {
       if (window.ethereum) {
 
-        if (useGSN) {
-          // with GSN
-          console.log("Sponsoring gas fee through gas station network~~");
-          const provider = await RelayProvider.newProvider({ provider: window.ethereum, gsnConfig }).init();
-          this.web3 = new Web3(provider);
-        } else {
-          // without GSN
-          this.web3 = new Web3(window.ethereum); //force it to version 1.3.0
-        }
-
+        this.web3 = new Web3(window.ethereum); //force it to version 1.3.0
         console.log("Current web3 version:",this.web3.version);
 
         let accounts = await this.web3.eth.getAccounts();
@@ -82,8 +59,6 @@ export default {
         //const contractAddress = "0xB84D7C7241E5C3acA5fBE63c12F40a3697891C64"; //ropsten testnet
         const contractAddress = "0x81E2cb23446745772b8cF4D3298DE054D84A4a27"; //mumbai testnet
         this.contract = await new this.web3.eth.Contract(VoteABI, contractAddress);
-        this.candidate1 = await this.contract.methods.getCandidateName(0).call();
-        this.candidate2 = await this.contract.methods.getCandidateName(1).call();
 
       } else {
         console.log("Please install Metamask to continue.");
@@ -99,9 +74,16 @@ export default {
       }, 1000);
     },
 
-    async vote(candidateID) {
-      await this.contract.methods.vote(candidateID).send({from:this.account});
-      console.log("Voted successfully!");
+    async register() {
+      console.log("Test: ", this.name);
+      const result = await this.contract.methods.register(this.name, this.nationalID).send({from:this.account});
+      if (result) {
+        console.log("Registered successfully");
+        this.display = "Successful!"
+        this.registered = true;
+      } else {
+        this.display = "Something went wrong, please try again.";
+      }
     }
 
   }
